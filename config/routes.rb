164e75d1e1ffda1de2ba143e52ac2071ab897dad
Nobuhiro Kawai <nkawai@chicagobooth.edu>
2016-12-15 09:28:00 -0600

    commit_1

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  root :to => "wants#index"
+
+  root "wants#index"
+
   # Routes for the Want_offer resource:
   # CREATE
   get "/want_offers/new", :controller => "want_offers", :action => "new"
